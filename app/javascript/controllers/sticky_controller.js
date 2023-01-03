8bde972e3ad0e2269aeedf06b0e1f10e8eb6cf87
Matthew Lang <matthewlang@hey.com>
2023-01-03 17:11:35 +0000

    We can change the color of the selector

--- app/javascript/controllers/sticky_controller.js
+++ app/javascript/controllers/sticky_controller.js
@@ -1,23 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "background", "selector" ]
 
   connect() {}
 
   change_color() {
-    if (this.outputTarget.classList.contains("bg-yellow-100")) {
-      this.outputTarget.classList.remove("bg-yellow-100");
-      this.outputTarget.classList.add("bg-red-100");
-    } else if (this.outputTarget.classList.contains("bg-red-100")) {
-      this.outputTarget.classList.remove("bg-red-100");
-      this.outputTarget.classList.add("bg-blue-100");
-    } else if (this.outputTarget.classList.contains("bg-blue-100")) {
-      this.outputTarget.classList.remove("bg-blue-100");
-      this.outputTarget.classList.add("bg-green-100");
+    if (this.backgroundTarget.classList.contains("bg-yellow-100")) {
+      // Switch from yellow to red
+      this.backgroundTarget.classList.remove("bg-yellow-100");
+      this.backgroundTarget.classList.add("bg-red-100");
+      this.selectorTarget.classList.remove("bg-red-300");
+      this.selectorTarget.classList.add("bg-blue-300");
+    } else if (this.backgroundTarget.classList.contains("bg-red-100")) {
+      // Switch from red to blue
+      this.backgroundTarget.classList.remove("bg-red-100");
+      this.backgroundTarget.classList.add("bg-blue-100");
+      this.selectorTarget.classList.remove("bg-blue-300");
+      this.selectorTarget.classList.add("bg-green-300");
+    } else if (this.backgroundTarget.classList.contains("bg-blue-100")) {
+      // Switch from blue to green
+      this.backgroundTarget.classList.remove("bg-blue-100");
+      this.backgroundTarget.classList.add("bg-green-100");
+      this.selectorTarget.classList.remove("bg-green-300");
+      this.selectorTarget.classList.add("bg-yellow-300");
     } else {
-      this.outputTarget.classList.remove("bg-green-100");
-      this.outputTarget.classList.add("bg-yellow-100");
+      // Switch from green to yellow
+      this.backgroundTarget.classList.remove("bg-green-100");
+      this.backgroundTarget.classList.add("bg-yellow-100");
+      this.selectorTarget.classList.remove("bg-yellow-300");
+      this.selectorTarget.classList.add("bg-red-300");
     }
   }
 }
