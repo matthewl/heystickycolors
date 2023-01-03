565b286149ee91a9cb103d420057c2ee7266bbcb
Matthew Lang <matthewlang@hey.com>
2023-01-03 19:25:57 +0000

    Safelist our sticky colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
+  safelist: [
+    'bg-red-100',
+    'bg-red-300',
+    'bg-green-100',
+    'bg-green-300',
+    'bg-blue-100',
+    'bg-blue-300',
+    'bg-yellow-100',
+    'bg-yellow-300'
+  ],
   theme: {
     extend: {
       fontFamily: {
