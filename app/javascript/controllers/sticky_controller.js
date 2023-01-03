a4f383a8d27b994d8f0669238e2aa4c9c2af3079
Matthew Lang <matthewlang@hey.com>
2023-01-03 17:47:21 +0000

    Refactor our sticky controller

--- app/javascript/controllers/sticky_controller.js
+++ app/javascript/controllers/sticky_controller.js
@@ -8,41 +8,43 @@ export default class extends Controller {
   change_color() {
     if (this.backgroundTarget.classList.contains("bg-yellow-100")) {
       // Switch from yellow to red
-      this.backgroundTarget.classList.remove("bg-yellow-100");
-      this.backgroundTarget.classList.add("bg-red-100");
-      this.selectorTarget.classList.remove("bg-red-300");
-      this.selectorTarget.classList.add("bg-blue-300");
+      this.changeBackground("yellow", "red");
+      this.changeSelector("red", "blue");
 
       // Update the sticky color in the database
       this.update_sticky(this.backgroundTarget.dataset.id, "red");
     } else if (this.backgroundTarget.classList.contains("bg-red-100")) {
       // Switch from red to blue
-      this.backgroundTarget.classList.remove("bg-red-100");
-      this.backgroundTarget.classList.add("bg-blue-100");
-      this.selectorTarget.classList.remove("bg-blue-300");
-      this.selectorTarget.classList.add("bg-green-300");
+      this.changeBackground("red", "blue");
+      this.changeSelector("blue", "green");
 
       // Update the sticky color in the database
       this.update_sticky(this.backgroundTarget.dataset.id, "blue");
     } else if (this.backgroundTarget.classList.contains("bg-blue-100")) {
       // Switch from blue to green
-      this.backgroundTarget.classList.remove("bg-blue-100");
-      this.backgroundTarget.classList.add("bg-green-100");
-      this.selectorTarget.classList.remove("bg-green-300");
-      this.selectorTarget.classList.add("bg-yellow-300");
+      this.changeBackground("blue", "green");
+      this.changeSelector("green", "yellow");
 
       // Update the sticky color in the database
       this.update_sticky(this.backgroundTarget.dataset.id, "green");
     } else {
       // Switch from green to yellow
-      this.backgroundTarget.classList.remove("bg-green-100");
-      this.backgroundTarget.classList.add("bg-yellow-100");
-      this.selectorTarget.classList.remove("bg-yellow-300");
-      this.selectorTarget.classList.add("bg-red-300");
+      this.changeBackground("green", "yellow");
+      this.changeSelector("yellow", "red");
 
       // Update the sticky color in the database
       this.update_sticky(this.backgroundTarget.dataset.id, "yellow");
     }
+  }
+
+  changeBackground(oldColor, newColor) {
+    this.backgroundTarget.classList.remove(`bg-${oldColor}-100`);
+    this.backgroundTarget.classList.add(`bg-${newColor}-100`);
+  }
+
+  changeSelector(oldColor, newColor) {
+    this.selectorTarget.classList.remove(`bg-${oldColor}-300`);
+    this.selectorTarget.classList.add(`bg-${newColor}-300`);
   }
 
   update_sticky(id, color) {
